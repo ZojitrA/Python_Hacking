531a452366b048b5bac3a13ccf37f3c8ac60a659
Soji <ahsojitra@gmail.com>
2019-03-09 15:46:01 -0500

    filtering packet :

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -1,9 +1,12 @@
 import scapy.all as scapy
+from scapy.layers import http
 
 def sniff(inferface):
     scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet)
 
 def process_sniffed_packet(packet):
-    print(packet)
+    if packet.haslayer(http.HTTPRequest):
+        if packet.haslayer(scapy.Raw):
+            print(packet[scapy.Raw].load)
 
 sniff("eth0")
